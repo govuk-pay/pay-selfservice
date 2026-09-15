7d5066d37487a4001d8ef2443ad3c03eb49c3a72
dan-searle-mt <dan.searle@madetech.com>
2026-09-15 11:11:47 +0100

    PP-15875: Added service file to mark task as complete

---
+++ src/services/adyen-setup.service.test.ts
@@ -0,0 +1,119 @@
+import sinon from 'sinon'
+import chaiAsPromised from 'chai-as-promised'
+import proxyquire from 'proxyquire'
+import chai from 'chai'
+
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
+const SERVICE_EXTERNAL_ID = 'service123abc'
+const ACCOUNT_TYPE = 'live'
+const CREDENTIAL_EXTERNAL_ID = 'adyen-credential-123-abc'
+
+describe('adyen setup service', function () {
+  describe('markTaskAsComplete', function () {
+    it('should patch the given task to COMPLETED using the connector client', async function () {
+      const patchStub = sinon.stub().resolves()
+
+      const ConnectorClientStub = function () {
+        return {
+          gatewayAccounts: {
+            adyenSetup: {
+              patch: patchStub,
+            },
+          },
+        }
+      }
+
+      const adyenSetupService = proxyquire('./adyen-setup.service', {
+        '@services/clients/pay/ConnectorClient.class': ConnectorClientStub,
+      })
+
+      await adyenSetupService.markTaskAsComplete(
+        SERVICE_EXTERNAL_ID,
+        ACCOUNT_TYPE,
+        CREDENTIAL_EXTERNAL_ID,
+        'organisationDetails'
+      )
+
+      const [serviceExternalId, accountType, credentialExternalId, updateRequest] = patchStub.firstCall.args
+      expect(serviceExternalId).to.equal(SERVICE_EXTERNAL_ID)
+      expect(accountType).to.equal(ACCOUNT_TYPE)
+      expect(credentialExternalId).to.equal(CREDENTIAL_EXTERNAL_ID)
+      expect(updateRequest.toJson()).to.deep.equal([
+        {
+          op: 'replace',
+          path: 'organisation_details',
+          value: 'COMPLETED',
+        },
+      ])
+    })
+
+    it('should map each task name to its connector path', async function () {
+      const cases = [
+        ['organisationDetails', 'organisation_details'],
+        ['legalTerms', 'legal_terms'],
+        ['bankDetails', 'bank_details'],
+        ['responsiblePerson', 'responsible_person'],
+        ['director', 'director'],
+        ['reasonForTakingPayments', 'reason_for_taking_payments'],
+      ]
+
+      for (const [taskName, expectedPath] of cases) {
+        const patchStub = sinon.stub().resolves()
+
+        const ConnectorClientStub = function () {
+          return {
+            gatewayAccounts: {
+              adyenSetup: {
+                patch: patchStub,
+              },
+            },
+          }
+        }
+
+        const adyenSetupService = proxyquire('./adyen-setup.service', {
+          '@services/clients/pay/ConnectorClient.class': ConnectorClientStub,
+        })
+
+        await adyenSetupService.markTaskAsComplete(SERVICE_EXTERNAL_ID, ACCOUNT_TYPE, CREDENTIAL_EXTERNAL_ID, taskName)
+
+        expect(patchStub.firstCall.args[3].toJson()).to.deep.equal([
+          {
+            op: 'replace',
+            path: expectedPath,
+            value: 'COMPLETED',
+          },
+        ])
+      }
+    })
+
+    it('should propagate errors from the connector client', async function () {
+      const error = new Error('Connector patch failed')
+      const patchStub = sinon.stub().rejects(error)
+
+      const ConnectorClientStub = function () {
+        return {
+          gatewayAccounts: {
+            adyenSetup: {
+              patch: patchStub,
+            },
+          },
+        }
+      }
+
+      const adyenSetupService = proxyquire('./adyen-setup.service', {
+        '@services/clients/pay/ConnectorClient.class': ConnectorClientStub,
+      })
+
+      await expect(
+        adyenSetupService.markTaskAsComplete(
+          SERVICE_EXTERNAL_ID,
+          ACCOUNT_TYPE,
+          CREDENTIAL_EXTERNAL_ID,
+          'organisationDetails'
+        )
+      ).to.be.rejectedWith(error)
+    })
+  })
+})
--- src/services/adyen-setup.service.ts
+++ src/services/adyen-setup.service.ts
@@ -1,3 +1,33 @@
-export async function markTaskAsComplete() {
-  // call client method
+import ConnectorClient from '@services/clients/pay/ConnectorClient.class'
+import { AdyenAccountSetupUpdateRequest } from '@models/gateway-account/AdyenAccountSetupUpdateRequest.class'
+import {
+  AdyenAccountSetupTaskName,
+  AdyenAccountSetupTaskStatus,
+} from '@models/gateway-account/AdyenAccountSetup.class'
+
+const connectorClient = new ConnectorClient()
+
+/**
+ * Function to call the connector client, to update the task status.
+ * @param serviceExternalId
+ * @param accountType
+ * @param credentialExternalId
+ * @param task
+ */
+export const markTaskAsComplete = async (
+  serviceExternalId: string,
+  accountType: string,
+  credentialExternalId: string,
+  task: AdyenAccountSetupTaskName
+) => {
+  const updateRequest = new AdyenAccountSetupUpdateRequest()
+    .replace()
+    [task](AdyenAccountSetupTaskStatus.COMPLETED)
+
+  await connectorClient.gatewayAccounts.adyenSetup.patch(
+    serviceExternalId,
+    accountType,
+    credentialExternalId,
+    updateRequest
+  )
 }
