e5e0f3cec4817bbda071ab9bb5dc1aa67f4459aa
dan-searle-mt <dan.searle@madetech.com>
2026-09-17 14:03:06 +0100

    PP-15823: Mark service director adyen task as completed (#5043)
    * PP-15823: Mark service director adyen task as completed
    
    * PP-15823: Fix cypress test mock

--- test/cypress/integration/simplified-account/service-settings/adyen-details/service-director/check-your-answers.cy.ts
+++ test/cypress/integration/simplified-account/service-settings/adyen-details/service-director/check-your-answers.cy.ts
@@ -56,6 +56,12 @@ const setStubs = (additionalStubs = []) => {
       ADYEN_CREDENTIAL_EXTERNAL_ID,
       adyenAccountSetup.toAdyenAccountSetupData().tasks
     ).success(),
+    GatewayAccountStubs.patchAdyenAccountTask(
+      SERVICE_EXTERNAL_ID,
+      LIVE_ACCOUNT_TYPE,
+      ADYEN_CREDENTIAL_EXTERNAL_ID,
+      'director'
+    ).success(),
     ...additionalStubs,
   ])
 }
